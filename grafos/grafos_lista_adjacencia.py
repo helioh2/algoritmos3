b33f17f6344f99e3bde2e4e3a3c34dc683442b30
Helio <heliohenrique3@gmail.com>
2021-12-03 11:52:23 -0300

    Grafos com matriz de adjacencia

--- grafos/grafos_lista_adjacencia.py
+++ grafos/grafos_lista_adjacencia.py
@@ -74,8 +74,128 @@ assert busca_profundidade(grafo1, "C", "B") == True
 assert busca_profundidade(grafo1, "C", "D") == False       
 
 
-def busca_largura(grafo, v_inicial, chave):
-    pass
+def busca_profundidade_rec_template(grafo, v_inicial, visitados):
+
+    # faz algo com v_inicial
+
+    visitados.append(v_inicial)
+
+    vizinhos = vizinhanca(grafo, v_inicial)
+
+    for v_proximo in vizinhos:
+        if v_proximo not in visitados:
+            resultado = busca_profundidade_rec_template(grafo, v_proximo, visitados)
+            # faz algo com resultado do vizinho
+
+
+
+def busca_profundidade_nao_rec_pilha(grafo, v_inicial):
+    visitados = set()  #conjunto
+    pilha = [v_inicial]
+
+    while pilha: ## enquanto pilha não está vazia
+        vertice = pilha.pop(-1)  #remove último (desempilhando)
+
+        # faz alguma coisa com vertice (ex: verifica se é o que está procurando)
+
+        if vertice not in visitados:
+            visitados.add(vertice)
+            vizinhos_nao_visitados = [v for v in vizinhanca(grafo, vertice) if v not in visitados]
+            pilha += vizinhos_nao_visitados #acrescenta vizinhos não visitados ao topo da pilha
+
+    return visitados
+
+grafo_aciclico = {
+    "A": ["B","C"],
+    "B": ["C", "D"],
+    "C": ["E"],
+    "D": ["G"],
+    "E": ["F"],
+    "F": [],
+    "G": []
+}
+
+
+print(busca_profundidade_nao_rec_pilha(grafo_aciclico, "A"))  
+
+
+def ordenacao_topologica(grafo):
+    """
+    DFS (busca em profundidade) modificada para retornar a ordenação topológica do grafo.
+    Isso é feito por meio da lista com os tempos de término do processamento
+    de cada vértice, que será a ordenação topológica reversa do grafo.
+    """
+
+    visitados = set()
+    ordem_topologica = {v: 0 for v in grafo.keys()}
+    tempo = 0
+
+    def busca_profundidade(grafo, v_inicial, visitados):
+
+        visitados.add(v_inicial)
+        vizinhos = vizinhanca(grafo, v_inicial)
+
+        for v_proximo in vizinhos:
+            if v_proximo not in visitados:
+                busca_profundidade(grafo, v_proximo, visitados)
+
+        nonlocal tempo
+        tempo += 1
+        
+        ordem_topologica[v_inicial] = tempo
+
+
+    for vertice in grafo:
+        if not vertice in visitados:
+            busca_profundidade(grafo, vertice, visitados)
+
+    return {v: abs(t-len(grafo)) for v,t in ordem_topologica.items()} # reverte ordem
+
+grafo_aciclico = {
+    "A": ["B","C"],
+    "B": ["C", "D"],
+    "C": ["E"],
+    "D": ["G"],
+    "E": ["F"],
+    "F": [],
+    "G": []
+}
+
+print(ordenacao_topologica(grafo_aciclico))
+
+
+def busca_largura_nao_rec_fila(grafo, v_inicial):
+    visitados = set()  #conjunto
+    fila = [v_inicial]
+
+    while fila: ## enquanto pilha não está vazia
+        vertice = fila.pop(0)  #remove primeiro (início da fila)
+
+        if vertice not in visitados:
+
+            print(vertice, end=", ")
+
+            visitados.add(vertice)
+            vizinhos_nao_visitados = [v for v in vizinhanca(grafo, vertice) if v not in visitados]
+            fila += vizinhos_nao_visitados #acrescenta vizinhos não visitados ao topo da pilha
+
+    return visitados
+
+print("Execução da busca em largura:")
+busca_largura_nao_rec_fila(grafo_aciclico, "A")
+
+
+def dikjstra(grafo, v_inicial):
+    visitados, fila = [], [v_inicial]
+
+    while fila:
+        vertice = fila.pop(0)
+        if vertice not in visitados:
+            visitados.append(vertice)
+            fila += [x for x in grafo[vertice] if x not in visitados]
+    return visitados
+
+# print(busca_largura(grafo1, "C"))
 
 
 def existe_caminho_entre(grafo, v1, v2):
--- grafos/grafos_matriz_adjacencia.py
+++ grafos/grafos_matriz_adjacencia.py
@@ -5,9 +5,23 @@ Usaremos 'None' para marcar inexistência de aresta e '1' para marcar existênci
 Necessário definir um vetor auxiliar (chaves) para mapear o valor da chave referente
 a cada linha/coluna
 '''
-grafo1 = [
-    [None,1,1],
-    [None,None,1],
-    [1,None,None]
-]
-chaves = [1,2,3]  # 0 -> 1; 1 -> 2; 2 -> 3
+from typing import Tuple
+
+
+class Grafo:
+
+    def __init__(self, vertices:list, arestas:Tuple[object,object,int]) -> None:
+        self.vertices = sorted(vertices)
+        self.matriz = [[None for _ in range(len(vertices))] for _ in range(len(vertices))]
+        
+        for v1, v2, peso in arestas:
+            i, j = vertices.index(v1), vertices.index(v2)
+            self.matriz[i][j] = peso
+
+
+vertices = ["A","B","C"]
+arestas = [("A","B",1), ("A","C",1), ("B","C",1), ("C","A",1)]
+
+grafo1 = Grafo(vertices, arestas)
+
+print(grafo1.matriz)
